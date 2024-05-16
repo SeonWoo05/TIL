f900fa4ae70694aa16f0fcec30f8b715643d464e
SeonWoo <tjsdn000508@gmail.com>
2024-05-16 13:55:56 +0900

    oj 에러종류

---
+++ Pyth/oj_errortype.py
@@ -0,0 +1,13 @@
+# Online Judge
+
+# AC: Accepted = 맞았습니다!!
+# CE: Compile Error = 컴파일 에러
+# MLE: Memory Limit Exceeded = 메모리 초과
+# PE: Presentation Error = 출력 형식이 잘못되었습니다
+# OLE: Output Limit Exceed = 출력 초과
+# RE: Runtime Error = 런타임 에러
+# TLE: Time Limit Exceeded = 시간 초과
+# WA: Wrong Answer = 틀렸습니다
+
+# 컴파일 에러는 컴파일 과정에서 발생하는 에러 (신택스 에러, 타입체크 에러)
+# 런타임 에러는 실행(런타임) 과정에서 발생하는 에러 (0나누기 에러, 널(unll)참조 오류, 메모리 부족 오류)
