b257a3c0cc4c16d8b623ff7c7d149bc1f350fb70
SeonWoo <tjsdn000508@gmail.com>
2024-05-30 10:22:05 +0900

    packing 추가

---
+++ Pyth/function.py
@@ -0,0 +1,99 @@
+## 함수
+
+# 리스트 넣어서 그 안의 원소들을 출력하는 함수
+def arg_list(food):
+    for x in food:
+        print(x)
+
+fruits = ["apple", "banana", "cherry"]
+# arg_list(fruits) 
+
+
+# 0~9까지 담긴 리스트 반환하는 함수
+def ret_list():
+    list = []
+    for i in range(0,10):
+        list.append(i)
+    return list
+# print(ret_list())
+
+
+# 딕셔너리와 key값을 입력하면 value를 반환하는 함수
+def arg_dict(dic,k):
+    return dic[k]
+
+majors = {"CS" : "Computer Science",
+         "EE" : "Electrical Engineering"}
+
+# print(arg_dict(majors, 'CS'))
+
+
+# 딕셔너리를 반환하는 함수
+def ret_dict():
+    d = dict()
+    d['str'] = "Tutorialspoint"
+    d['x'] = 50
+    return d
+
+# print(ret_dict())
+
+
+## 정의 위치에 따른 변수의 종류
+
+# test() : UnboundLocalError 발생
+a = 17
+def test():
+    print(a)
+    a = 13
+    print(a)
+
+
+# Variable scope : 해당 변수가 접근할 수 있는 변수, 객체 그리고 함수의 집합
+# Scope resolution : LEGB rule
+
+# Global scope
+x = 0
+def outer():
+    # Enclosed scope : 중첩된 함수가 있을 때 적용되는 scope
+    x = 1
+    print(f'Enclosed scope : {x}')
+    def inner():
+        # Local scope
+        x = 2
+        print(f'Local scope : {x}')
+    inner()
+
+# print(f'Global scope : {x}')
+# outer()
+
+
+# 가변적인 인자전달
+# *을 매개변수의 앞에 넣어 사용 : operator을 사용한 unpacking
+def greet(*names):
+    for name in names:
+        print(name)
+
+greet('A','B','C')
+greet('1','2')
+
+# 숫자의 합을 구하는 프로그램
+def sum_nums(*numbers): # 전체를 튜플형식으로 받아옴
+    result = 0
+    for n in numbers:
+        result += n
+    return result
+print(sum_nums(10,20,30,40))
+
+
+## Packing
+a = [2,3]
+b,c = a
+print(b)
+
+a = {'a':'apple', 'b':'banana'}
+b,c = a
+print(b,c)
+
+b,c = a.values()
+print(b,c)
+
