b9a32e4b0fd3bbe31387a78b67d296f0ecf1d276
SeonWoo <tjsdn000508@gmail.com>
2024-05-31 21:53:22 +0900

    dictionary 풀이 추가

--- Pyth/Quiz5_reveiw.py
+++ Pyth/Quiz5_reveiw.py
@@ -1,3 +1,22 @@
+# list comprehension
 nums = list(map(int, input().split()))
 A = [x for x in nums if nums.count(x) == 1]
 print(*A)
+
+
+# dictionary
+nums = list(map(int, input().split()))
+
+freq = {} # 빈도수를 저장할 딕셔너리 생성
+
+for num in nums: # 반복문으로 freq 안에 num이 있으면 value를 +1 / 없으면 value를 =1 로 설정
+    if num in freq:
+        freq[num] += 1
+    else:
+        freq[num] = 1
+
+
+for num in freq: # 빈도수가 1인 원소 찾기
+    if freq[num] == 1:
+        print(num)
+        break
